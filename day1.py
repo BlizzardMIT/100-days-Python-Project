6cb38cf7f44872ab9d0567550402935a7ac159a1
BlizzardMIT <56599303+BlizzardMIT@users.noreply.github.com>
2023-07-16 23:23:50 +1000

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,6 @@
+#Managing data with variables for Python naming rules
+print("Create a Name for Your Band!")
+city = input("What city were you born in?\n")
+number = input("What is the number of your band?\n")
+place = input("What is the special significance to your band?\n")
+print ("Your band is " + city+ ""+ number + place)
