83c9ff5ac76bbcfe1c345390ea1058ac4f5a3e1e
BlizzardMIT <56599303+BlizzardMIT@users.noreply.github.com>
2023-07-19 01:54:51 +1000

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -1,12 +1,17 @@
+#import the necessary module
 import random
-letters = ['alphabet']
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 symbols = ['!','@','#','$','%','^','&','*']
 
-print("PythonPassword Generator!")
+print("Welcome to Password Generator!")
+
+#input the length of password
 nr_letters = (input("How many letters would you like to choose in your password?\n"))
 nr_numbers = (input(f"How many numbers would you like to choose?\n"))
-nr_symbols = input(f"How many symbols would you like to choose?\n")
+nr_symbols = (input(f"How many symbols would you like to choose?\n"))
 
 if not nr_letters.isdigit() or not nr_numbers.isdigit() or not nr_symbols.isdigit():
     print("Invalid value, please enter a number instead.")
@@ -34,7 +39,7 @@ print(f"Your password: {''.join(password)}")
 if len(password) <= 5:
     print("Your password is too weak, try to include at least 8 characters for a stronger password.")
 
-elif len(password) == 7:
+elif len(password) == 9:
     print("Your password is medium, try to include at least 8 characters for a stronger password.")
 
 else:
