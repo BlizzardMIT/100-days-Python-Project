8fd8a37fc1931a216ddd47b35b929a43c6545aa3
BlizzardMIT <56599303+BlizzardMIT@users.noreply.github.com>
2023-07-19 01:54:20 +1000

    Update Tip_calculator.py

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -1,8 +1,8 @@
-print("Tip calculator!")
+print("Welcome to the tip calculator!")
 
-bill = float(input("What is the total bill amount?\n$:"))
+bill = float(input("What is the total bill?\n$:"))
 tip = int(input("How much tip would you like to give?\nPercent:"))
-split = int(input("How many peopple to split the bill?\nPeople:"))
+split = int(input("How many peopple are splitting the bill?\nPeople:"))
 
 total = ("{:.2f}".format ((((bill * (tip/100)) + bill)  / split)))
 
